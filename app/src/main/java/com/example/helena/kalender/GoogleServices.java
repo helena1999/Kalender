a91eebd2fa6572bbd5c6997e425d5cfb5d47e32f
ERPLY <guido@erply.com>
2018-06-06 18:53:57 +0300

    12

--- app/src/main/java/com/example/helena/kalender/GoogleServices.java
+++ app/src/main/java/com/example/helena/kalender/GoogleServices.java
@@ -63,10 +63,7 @@ public class GoogleServices extends Activity implements EasyPermissions.Permissi
     private static final String PREF_ACCOUNT_NAME = "accountName";
     private static final String[] SCOPES = { CalendarScopes.CALENDAR_READONLY };
 
-    /**
-     * Create the main activity.
-     * @param savedInstanceState previously saved instance data.
-     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -117,13 +114,7 @@ public class GoogleServices extends Activity implements EasyPermissions.Permissi
 
 
 
-    /**
-     * Attempt to call the API, after verifying that all the preconditions are
-     * satisfied. The preconditions are: Google Play Services installed, an
-     * account was selected and the device currently has online access. If any
-     * of the preconditions are not satisfied, the app will prompt the user as
-     * appropriate.
-     */
+
     private void getResultsFromApi() {
         if (! isGooglePlayServicesAvailable()) {
             acquireGooglePlayServices();
@@ -132,8 +123,7 @@ public class GoogleServices extends Activity implements EasyPermissions.Permissi
         } else if (! isDeviceOnline()) {
             mOutputText.setText("No network connection available.");
         } else {
-            // This is success @debug
-            //new MakeRequestTask(mCredential).execute();
+
             Log.v("KALENDAR", mCredential.getSelectedAccountName());
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             intent.putExtra("login", true);
@@ -143,16 +133,7 @@ public class GoogleServices extends Activity implements EasyPermissions.Permissi
         }
     }
 
-    /**
-     * Attempts to set the account used with the API credentials. If an account
-     * name was previously saved it will use that one; otherwise an account
-     * picker dialog will be shown to the user. Note that the setting the
-     * account to use with the credentials object requires the app to have the
-     * GET_ACCOUNTS permission, which is requested here if it is not already
-     * present. The AfterPermissionGranted annotation indicates that this
-     * function will be rerun automatically whenever the GET_ACCOUNTS permission
-     * is granted.
-     */
+
     @AfterPermissionGranted(REQUEST_PERMISSION_GET_ACCOUNTS)
     private void chooseAccount() {
         if (EasyPermissions.hasPermissions(
@@ -163,13 +144,13 @@ public class GoogleServices extends Activity implements EasyPermissions.Permissi
                 mCredential.setSelectedAccountName(accountName);
                 getResultsFromApi();
             } else {
-                // Start a dialog from which the user can choose an account
+
                 startActivityForResult(
                         mCredential.newChooseAccountIntent(),
                         REQUEST_ACCOUNT_PICKER);
             }
         } else {
-            // Request the GET_ACCOUNTS permission via a user dialog
+
             EasyPermissions.requestPermissions(
                     this,
                     "This app needs to access your Google account (via Contacts).",
@@ -178,16 +159,7 @@ public class GoogleServices extends Activity implements EasyPermissions.Permissi
         }
     }
 
-    /**
-     * Called when an activity launched here (specifically, AccountPicker
-     * and authorization) exits, giving you the requestCode you started it with,
-     * the resultCode it returned, and any additional data from it.
-     * @param requestCode code indicating which activity result is incoming.
-     * @param resultCode code indicating the result of the incoming
-     *     activity result.
-     * @param data Intent (containing result data) returned by incoming
-     *     activity result.
-     */
+
     @Override
     protected void onActivityResult(
             int requestCode, int resultCode, Intent data) {
@@ -226,14 +198,7 @@ public class GoogleServices extends Activity implements EasyPermissions.Permissi
         }
     }
 
-    /**
-     * Respond to requests for permissions at runtime for API 23 and above.
-     * @param requestCode The request code passed in
-     *     requestPermissions(android.app.Activity, String, int, String[])
-     * @param permissions The requested permissions. Never null.
-     * @param grantResults The grant results for the corresponding permissions
-     *     which is either PERMISSION_GRANTED or PERMISSION_DENIED. Never null.
-     */
+
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String[] permissions,
@@ -243,34 +208,19 @@ public class GoogleServices extends Activity implements EasyPermissions.Permissi
                 requestCode, permissions, grantResults, this);
     }
 
-    /**
-     * Callback for when a permission is granted using the EasyPermissions
-     * library.
-     * @param requestCode The request code associated with the requested
-     *         permission
-     * @param list The requested permission list. Never null.
-     */
+
     @Override
     public void onPermissionsGranted(int requestCode, List<String> list) {
-        // Do nothing.
+
     }
 
-    /**
-     * Callback for when a permission is denied using the EasyPermissions
-     * library.
-     * @param requestCode The request code associated with the requested
-     *         permission
-     * @param list The requested permission list. Never null.
-     */
+
     @Override
     public void onPermissionsDenied(int requestCode, List<String> list) {
-        // Do nothing.
+
     }
 
-    /**
-     * Checks whether the device currently has a network connection.
-     * @return true if the device has a network connection, false otherwise.
-     */
+
     private boolean isDeviceOnline() {
         ConnectivityManager connMgr =
                 (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -278,11 +228,7 @@ public class GoogleServices extends Activity implements EasyPermissions.Permissi
         return (networkInfo != null && networkInfo.isConnected());
     }
 
-    /**
-     * Check that Google Play services APK is installed and up to date.
-     * @return true if Google Play Services is available and up to
-     *     date on this device; false otherwise.
-     */
+
     private boolean isGooglePlayServicesAvailable() {
         GoogleApiAvailability apiAvailability =
                 GoogleApiAvailability.getInstance();
@@ -291,10 +237,7 @@ public class GoogleServices extends Activity implements EasyPermissions.Permissi
         return connectionStatusCode == ConnectionResult.SUCCESS;
     }
 
-    /**
-     * Attempt to resolve a missing, out-of-date, invalid or disabled Google
-     * Play Services installation via a user dialog, if possible.
-     */
+
     private void acquireGooglePlayServices() {
         GoogleApiAvailability apiAvailability =
                 GoogleApiAvailability.getInstance();
@@ -306,12 +249,7 @@ public class GoogleServices extends Activity implements EasyPermissions.Permissi
     }
 
 
-    /**
-     * Display an error dialog showing that Google Play Services is missing
-     * or out of date.
-     * @param connectionStatusCode code describing the presence (or lack of)
-     *     Google Play Services on this device.
-     */
+
     void showGooglePlayServicesAvailabilityErrorDialog(
             final int connectionStatusCode) {
         GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
@@ -322,10 +260,7 @@ public class GoogleServices extends Activity implements EasyPermissions.Permissi
         dialog.show();
     }
 
-    /**
-     * An asynchronous task that handles the Google Calendar API call.
-     * Placing the API calls in their own task ensures the UI stays responsive.
-     */
+
     private class MakeRequestTask extends AsyncTask<Void, Void, List<String>> {
         private com.google.api.services.calendar.Calendar mService = null;
         private Exception mLastError = null;
@@ -339,10 +274,7 @@ public class GoogleServices extends Activity implements EasyPermissions.Permissi
                     .build();
         }
 
-        /**
-         * Background task to call Google Calendar API.
-         * @param params no parameters needed for this task.
-         */
+
         @Override
         protected List<String> doInBackground(Void... params) {
             try {
@@ -354,13 +286,9 @@ public class GoogleServices extends Activity implements EasyPermissions.Permissi
             }
         }
 
-        /**
-         * Fetch a list of the next 10 events from the primary calendar.
-         * @return List of Strings describing returned events.
-         * @throws IOException
-         */
+
         private List<String> getDataFromApi() throws IOException {
-            // List the next 10 events from the primary calendar.
+
             DateTime now = new DateTime(System.currentTimeMillis());
             List<String> eventStrings = new ArrayList<String>();
             Events events = mService.events().list("primary")
@@ -374,8 +302,7 @@ public class GoogleServices extends Activity implements EasyPermissions.Permissi
             for (Event event : items) {
                 DateTime start = event.getStart().getDateTime();
                 if (start == null) {
-                    // All-day events don't have start times, so just use
-                    // the start date.
+
                     start = event.getStart().getDate();
                 }
                 eventStrings.add(
